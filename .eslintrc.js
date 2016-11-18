4dc16b26cd8c64b45051ee176fcc7f111ee5a154
nikches <nikches@angelsit.ru>
2016-11-18 15:14:40 +0300

    Added .editorconfig, .eslintrc.js, .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-unused-vars": 0
+    }
+};
