2aa95536ee831fe6b1508a179bc6d3b90bdc7828
nikches <nikches@angelsit.ru>
2016-11-21 17:38:52 +0300

    Enabled support of ES6 and JSx in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+        "es6": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
         "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
         "linebreak-style": [
             "error",
             "unix"
         ],
-        "no-unused-vars": 0
+		"no-unused-vars": 0,
     }
 };
