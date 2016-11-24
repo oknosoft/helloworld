154c403a0d38a0993eede72007431af72d6a39ef
unpete <info@oknosoft.ru>
2016-11-24 22:25:21 +0500

    call metadata-abstract-ui in /src/metadata/index.js

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -3,23 +3,24 @@
 // конструктор metadata.js
 //const MetaEngine = require('metadata-core').default
 
-import MetaEngine from 'metadata-core'
-import metadata_pouchdb from 'metadata-pouchdb'
-import metadata_redux from 'metadata-redux'
-import metadata_react from 'metadata-react-ui/plugin'
+import MetaEngine from 'metadata-core';
+import metadata_pouchdb from 'metadata-pouchdb';
+import metadata_redux from 'metadata-redux';
+import metadata_ui from 'metadata-abstract-ui';
 
 MetaEngine
-  .plugin(metadata_pouchdb)       // подключаем pouchdb-адаптер к прототипу metadata.js
-  .plugin(metadata_redux)         // подключаем свойства redux к прототипу metadata.js
-  .plugin(metadata_react)         // подключаем методы для работы с компонентами react к прототипу metadata.js
+  .plugin(metadata_pouchdb)     // подключаем pouchdb-адаптер к прототипу metadata.js
+  .plugin(metadata_redux)       // подключаем свойства redux к прототипу metadata.js
+  .plugin(metadata_ui)          // подключаем методы для работы с компонентами react к прототипу metadata.js
 
 const $p = new MetaEngine()
 export default $p
 
 // в отладочном режиме экспортируем $p глобально
 if (__DEBUG__) {
-  window.$p = $p;
+  window.$p = $p
   //noinspection
-  if(!window.PouchDB)
-    window.PouchDB = $p.classes.PouchDB;
+  if(!window.PouchDB){
+	  window.PouchDB = $p.classes.PouchDB
+  }
 }
