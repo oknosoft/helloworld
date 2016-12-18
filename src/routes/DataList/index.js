0584b8de1739616c4d9d4413dd5822a839d6e2e3
unpete <info@oknosoft.ru>
2016-12-18 21:35:25 +0500

    babel preset es2016

--- src/routes/DataList/index.js
+++ src/routes/DataList/index.js
@@ -7,7 +7,7 @@ export default (store) => ({
 	getComponent (nextState, cb) {
 		/*  Webpack - use 'require.ensure' to create a split point
 		 and embed an async module loader (jsonp) when bundling   */
-		require.ensure([], (require) => {
+		require.ensure([], function(require) {
 
 			/*  Webpack - use require callback to define
 			 dependencies for bundling   */
