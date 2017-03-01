59a5ea06df82cf22cb46d13a17a672af08e375d2
nikches <nikches@angelsit.ru>
2017-03-01 11:54:46 +0300

    Remove app.settings.js and metadata.config.js from git index.

--- config/metadata.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * This file override default configuration in metadata.config.default.js
- */
-const metadataConfigDefault = require("./metadata.config.default")
-const metadataConfig = {
-  // Insert your configuration here
-}
-
-module.exports = Object.assign({}, metadataConfigDefault, metadataConfig)
