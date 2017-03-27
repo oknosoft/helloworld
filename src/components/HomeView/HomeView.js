743f5d7047e978f82a8f861b0e69fa0211c999f7
nikches <nikches@angelsit.ru>
2017-03-27 15:02:12 +0300

    Removed AutoSizer from CoreLayout.

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -6,14 +6,12 @@ import DataList from "metadata-ui/DataList";
 export default class HomeView extends MetaComponent {
 
   static propTypes = {
-    width: React.PropTypes.number.isRequired,
-    height: React.PropTypes.number.isRequired
   }
 
   render() {
     const {props, context} = this
     const {$p} = context
 
-    return <DataList { ...props } _mgr={$p.doc.cash_moving}/>
+    return <DataList { ...props } _mgr={$p.doc.cash_moving} />
   }
 }
