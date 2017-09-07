104316474f9b1ef2a8570d8d19858d8c7c981aba
unpete <info@oknosoft.ru>
2017-09-07 12:58:47 +0500

    BrowserCompatibility

--- src/components/App/RootView.js
+++ src/components/App/RootView.js
@@ -12,6 +12,9 @@ import DumbScreen from '../DumbScreen';
 // корневые контейнеры
 import AppView from './AppView';
 
+import browser_compatible from 'metadata-react/BrowserCompatibility/browser_compatible';
+import BrowserCompatibility from 'metadata-react/BrowserCompatibility';
+
 // тема для material-ui
 import {MuiThemeProvider} from 'material-ui/styles';
 import theme from '../../styles/muiTheme';
@@ -20,7 +23,10 @@ class RootView extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {path_log_in: this.isPathLogIn()};
+    this.state = {
+      path_log_in: this.isPathLogIn(),
+      browser_compatible: browser_compatible(),
+    };
   }
 
   shouldComponentUpdate(props, state) {
@@ -28,19 +34,19 @@ class RootView extends Component {
     const {path_log_in} = state;
     let res = true;
 
-    if (path_log_in != this.isPathLogIn()) {
+    if(path_log_in != this.isPathLogIn()) {
       this.setState({path_log_in: this.isPathLogIn()});
       res = false;
     }
 
     // если есть сохранённый пароль и online, пытаемся авторизоваться
-    if (!user.logged_in && user.has_login && !user.try_log_in && !offline) {
+    if(!user.logged_in && user.has_login && !user.try_log_in && !offline) {
       props.handleLogin();
       res = false;
     }
 
     // если это первый запуск или couch_direct и offline, переходим на страницу login
-    if (!path_log_in && ((data_empty === true && !user.try_log_in) || (couch_direct && offline))) {
+    if(!path_log_in && ((data_empty === true && !user.try_log_in) || (couch_direct && offline))) {
       history.push('/login');
       res = false;
     }
@@ -54,7 +60,7 @@ class RootView extends Component {
 
   render() {
 
-    const {props} = this;
+    const {props, state} = this;
     const {meta_loaded, data_empty, data_loaded, history} = props;
     const show_dumb = !meta_loaded ||
       (data_empty === undefined) ||
@@ -62,12 +68,15 @@ class RootView extends Component {
 
     return <MuiThemeProvider theme={theme}>
       {
-        show_dumb ?
-          <DumbScreen {...props} />
+        state.browser_compatible ?
+          (show_dumb ?
+            <DumbScreen {...props} />
+            :
+            <Router history={history}>
+              <Route component={AppView}/>
+            </Router>)
           :
-          <Router history={history}>
-            <Route component={AppView}/>
-          </Router>
+          (<BrowserCompatibility/>)
       }
     </MuiThemeProvider>;
   }
