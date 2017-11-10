f74ab621499dafb66b775aa25667aff3b7f04773
unpete <info@oknosoft.ru>
2017-11-10 11:46:54 +0500

    забытый Home

---
+++ src/pages/Home/Footer.js
@@ -0,0 +1,67 @@
+// @flow
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+
+const styleSheet = theme => ({
+  root: {
+    overflow: 'auto',
+  },
+  layout: {
+    padding: theme.spacing.unit * 6,
+  },
+  list: {
+    margin: 0,
+    paddingLeft: 0,
+    listStyle: 'none',
+  },
+  listItem: {
+    paddingTop: theme.spacing.unit / 2,
+    paddingBottom: theme.spacing.unit / 2,
+  },
+});
+
+function AppFooter(props) {
+  const { classes } = props;
+
+  return (
+    <footer className={classes.root}>
+      <div className={classes.layout}>
+        <Typography type="title" gutterBottom>Быстрые ссылки</Typography>
+        <Typography type="subheading" component="div">
+          <Grid container spacing={0}>
+            <Grid item xs={12} sm={6}>
+              <ul className={classes.list}>
+                <li className={classes.listItem}>
+                  <a href="https://github.com/oknosoft/helloworld" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </li>
+                <li className={classes.listItem}>
+                  <a href="https://www.facebook.com/oknosoft/" target="_blank" rel="noopener noreferrer">Facebook</a>
+                </li>
+              </ul>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <ul className={classes.list}>
+                <li className={classes.listItem}>
+                  <a href="https://github.com/oknosoft/metadata.js" target="_blank" rel="noopener noreferrer">Metadata.js</a>
+                </li>
+                <li className={classes.listItem}>
+                  <a href="https://github.com/oknosoft/flowcon" target="_blank" rel="noopener noreferrer">Дорожная карта</a>
+                </li>
+              </ul>
+            </Grid>
+          </Grid>
+        </Typography>
+      </div>
+    </footer>
+  );
+}
+
+AppFooter.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styleSheet)(AppFooter);
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,54 @@
+// @flow
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Helmet from 'react-helmet';
+import AppFooter from './Footer';
+import styles from './styles';
+
+const ltitle = 'Демо metadata.js';
+const htitle = 'Metadata hello world';
+
+function PageHome(props) {
+  const {classes, handleNavigate, title} = props;
+
+  if(title != ltitle) {
+    props.handleIfaceState({
+      component: '',
+      name: 'title',
+      value: ltitle,
+    });
+  }
+
+  return (
+    <div className={classes.root}>
+      <Helmet title={htitle}/>
+
+      <div className={classes.hero}>
+        <div className={classes.content}>
+
+          <div className={classes.text}>
+            <Typography type="display1" component="h1" color="inherit" noWrap style={{marginBottom: 24}}>Metadata hello world</Typography>
+            <Typography type="subheading" component="h2" color="inherit" className={classes.headline}>
+              Каркас приложения на metadata.js<br/> для демонстрации базовых возможностей фреймворка
+            </Typography>
+            <Button className={classes.button} raised onClick={() => handleNavigate('/help')}>Начать</Button>
+          </div>
+        </div>
+      </div>
+      <AppFooter/>
+    </div>
+  );
+}
+
+PageHome.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  handleNavigate: PropTypes.func.isRequired,
+  handleIfaceState: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(PageHome);
