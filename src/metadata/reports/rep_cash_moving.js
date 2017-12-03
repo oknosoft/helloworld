e37b53b7bac0968134628f2505503a8433ec52ba
unpete <info@oknosoft.ru>
2017-12-04 00:27:49 +0500

    2.0.16-beta.42

--- src/metadata/reports/rep_cash_moving.js
+++ src/metadata/reports/rep_cash_moving.js
@@ -137,7 +137,7 @@ export default function ($p) {
      * Фильтр по отбору схемы + свёртка по измерениям + группировка по схеме
      */
     calculate() {
-      const {data, scheme, resources, _manager} = this;
+      const {data, scheme, _manager} = this;
       const _columns = scheme.rx_columns({
         mode: 'ts',
         fields: _manager.metadata('data').fields,
@@ -159,159 +159,10 @@ export default function ($p) {
           // фильтруем результат с учетом разыменования и видов сравнения
           scheme.filter(data, '', true);
 
-          // сворачиваем результат и сохраняем его в data._rows
-          const dims = scheme.dims();
-          const ress = [];
-          _columns.forEach(({key}) => {
-            if(dims.indexOf(key) == -1 && resources.indexOf(key) != -1) {
-              ress.push(key);
-            }
-            else {
-              // для базовой группировки, подмешиваем в измерения всё, что не ресурс
-              dims.indexOf(key) == -1 && dims.push(key);
-            }
-          });
-
-          // группируем по схеме
-
-          // TODO сейчас поддержана только первая запись иерархии
-
-          // TODO сейчас нет понятия детальных записей - всё сворачивается по измерениям
-
-          // TODO сейчас набор полей не поддержан в интерфейсе, но решаем сразу для группировки по нескольким полям
-          const grouping = scheme.dims();
-          if(grouping.length) {
-            // строка полей группировки без пустых полей
-            const dflds = dims.filter(v => v).join(', ');
-
-            // TODO в группировке может потребоваться разыменовать поля
-
-            // TODO итоги надо считать не по всем русурсам
-
-            // TODO итоги надо считать с учетом формулы
-
-            const sql = `select ${ress.map(res => `sum(${res}) as ${res}`).join(', ')
-              } ${dflds ? ((ress.length ? ', ' : '') + dflds) : ''} from ? ${dflds ? 'group by ROLLUP(' + dflds + ')' : ''}`;
-
-            // TODO еще, в alasql есть ROLLUP, CUBE и GROUPING SETS (аналог 1С-ного ИТОГИ) - можно задействовать
-            const res = $p.wsql.alasql(sql, [data._obj]);
-
-            // складываем результат в иерархическую структуру
-            const levels = [];
-            let index = 0;
-            for (const row of res) {
-
-              row.row = (index++).toString();
-
-              // является ли строка группировкой?
-              const is_group = dims.some(v => row[v] === null);
-              const parent_group = {};
-              const parent_dims = [];
-              is_group && dims.forEach(v => {
-                if(row[v] !== null) {
-                  parent_dims.push(v);
-                  parent_group[v] = row[v];
-                }
-              });
-
-              // ищем подходящего родителя
-              let parent;
-              for (const level of levels) {
-                if(is_group) {
-                  const lim = parent_dims.length - 1;
-                  if(parent_dims.every((v, i) => (level[v] instanceof Date && row[v] instanceof Date ?
-                      level[v].valueOf() === row[v].valueOf() : level[v] === row[v]) || i === lim)) {
-                    parent = level;
-                  }
-                  // если мы группировка, добавляем себя в levels
-                  row.children = [];
-                  levels.push(row);
-                  break;
-                }
-                else {
-                  const lim = dims.length - 1;
-                  if(dims.every((v, i) => level[v] === row[v] || i === lim)) {
-                    parent = level;
-                    break;
-                  }
-                }
-              }
-              if(parent) {
-                parent.children.push(row);
-              }
-              else {
-                if(!levels.length) {
-                  row.children = [];
-                  levels.push(row);
-                }
-              }
-            }
-
-            data._rows._count = index;
-            if(grouping[0]){
-              for(const level of levels[0].children){
-                data._rows.push(level);
-              }
-              data._rows._count -= 1;
-            }
-            else{
-              data._rows.push(levels[0]);
-            }
-
-            // TODO для ссылочных полей надо выполнить приведение типов, т.к в alasql возвращает guid`s вместо объектов
-            this.cast(data._rows, 0, dims);
-
-          }
-          else {
-            // или заполняем без группировки
-            data.group_by(dims, ress);
-            data.forEach((row) => {
-              data._rows.push(row);
-            });
-            data._rows._count = data._rows.length;
-          }
+          // группируем по схеме - сворачиваем результат и сохраняем его в ._rows
+          scheme.group_by(data);
 
         });
-    }
-
-    /**
-     * Выполняет приведение типов в группировках и ссылочных полях после alasql
-     * TODO: переместить в компоновку
-     */
-    cast(rows, level, dims, dim, meta) {
-      if(!meta){
-        meta = this._metadata(this._manager._tabular || 'data').fields;
-        dim = dims[dims.length - 1];
-      }
-      const {utils} = $p;
-      for(const row of rows) {
-        if(row.children){
-          // если это группировка верхнего уровня
-          if(level == 0){
-            row[dim] = meta[dim].type.is_ref ? {presentation: 'Σ'} : 'Σ';
-          }
-          else{
-            const gdim = dims[level];
-            const mgr = gdim && this._manager.value_mgr(row, gdim, meta[gdim].type);
-            const val = mgr && utils.is_data_mgr(mgr) ? mgr.get(row[gdim]) : row[gdim];
-            row[dim] = this._manager.value_mgr(row, dim, meta[dim].type) ?
-              (
-                utils.is_data_obj(val) ? val : {presentation: val instanceof Date ? utils.moment(val).format(utils.moment._masks[meta[gdim].type.date_part]) : val }
-              )
-              :
-              (
-                utils.is_data_obj(val) ? val.toString() : val
-              );
-          }
-          this.cast(row.children, level + 1, dims, dim, meta);
-        }
-        else{
-          const mgr = this._manager.value_mgr(row, dim, meta[dim].type);
-          if(utils.is_data_mgr(mgr)) {
-            row[dim] =  mgr.get(row[dim]);
-          }
-        }
-      }
     }
 
   };
