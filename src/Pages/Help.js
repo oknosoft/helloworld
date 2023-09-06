1f57c68c75ae13c38ebcb1a5d92f453278f31770
unpete <info@oknosoft.ru>
2023-09-06 15:11:07 +0500

    initial commit

---
+++ src/Pages/Help.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MarkdownDocs from 'metadata-ui/Markdown/MarkdownDocs';
+import {useMatches } from 'react-router-dom';
+import {useTitleContext} from '../App';
+
+const opt = {baseUrl: '/help/'};
+export default function HelpRoute(props) {
+  const {description, title, setTitle} = useTitleContext();
+  const matches = useMatches();
+  const {pathname} = matches[matches.length-1];
+  const [markdown, setMarkdown] = React.useState('Получаем файл с сервера...');
+
+  let fname = pathname.replace('/help', '');
+  if(fname.startsWith('/')) {
+    fname = fname.substring(1);
+  }
+
+  React.useEffect(() => {
+    fetch(`${$p.job_prm.docs_root}${fname || 'index.md'}`)
+      .then((res) => res.text())
+      .then((markdown) => setMarkdown(markdown))
+      .catch((err) => setMarkdown(err.message || err));
+  }, [fname]);
+
+  return <MarkdownDocs
+    {...props}
+    markdown={markdown}
+    opt={opt}
+    setTitle={setTitle}
+  />;
+}
---
+++ src/Pages/Page.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import MarkdownDocs from 'metadata-ui/Markdown/MarkdownDocs';
+import {useMatches } from 'react-router-dom';
+import {useTitleContext} from '../App';
+
+export default function Page(props) {
+  const {description, title, setTitle} = useTitleContext();
+  const matches = useMatches();
+  const {pathname} = matches[matches.length-1];
+  const [markdown, setMarkdown] = React.useState('');
+
+  React.useEffect(() => {
+    import(`.${pathname}.md`)
+      .then((module) => {
+        setMarkdown(module.default);
+      });
+  }, [pathname]);
+
+  return <MarkdownDocs
+    {...props}
+    markdown={markdown}
+    //subtitle="Окнософт"
+    //descr={description}
+    setTitle={setTitle}
+  />;
+}
